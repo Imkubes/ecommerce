461dacfce8bf9423c35a006893e5ecd50a130789
Imkubes <99090378+Imkubes@users.noreply.github.com>
2022-05-01 22:07:31 -0500

    Models almost done, getting confused on the relationship part

--- models/index.js
+++ models/index.js
@@ -3,6 +3,18 @@ const Product = require('./Product');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+Product.belongsTo(Category, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+  });
 
+Category.hasMany(Product, {
+    foreignKey: 'category_id',
+    onDelete: 'CASCADE',
+});
+
+Product.hasMany(Tag, {
+    foreignKey: ''
+})
 
 module.exports = { Category, Product, Tag, ProductTag };
